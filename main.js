0d3bb61066a09ae8f4adefc5b00b0cb92c907057
goakingFy <joaquindiaz66@gmail.com>
2022-10-09 20:42:03 +0200

    agregado funciones de resultados

--- main.js
+++ main.js
@@ -1,7 +1,29 @@
 
-// const getData = async ()=>{
-//         let data = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=2&sparkline=false")
-//         let res = await data.json()
-//         console.log(res)
-// }
-// getData()
+import {$container_results} from './varDOM.js';
+ 
+
+ const getAllCoin = async (type,page,maxResults)=>{
+         let data = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${type}&order=market_cap_desc&per_page=${maxResults}&page=${page}&sparkline=false1`)
+        let res = await data.json()
+        res.forEach(results => {
+            printResult(results.symbol,results.image)
+        });
+         
+ }
+ getAllCoin("usd" , "1", "100")
+
+ function printResult(name,img){
+    
+    let newReslt = document.createElement('div')
+    newReslt.classList.add("result")
+    newReslt.innerHTML = `
+    <span><img src="${img}" alt="img_${name}" class="thumbnail"> ${name}</span>
+    <button class="btn_addLove far fa-heart "></button>
+    `
+    $container_results.appendChild(newReslt)
+    
+ }
+
+
+ 
+
