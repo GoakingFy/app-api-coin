f60d99750fe1aabc987cc9b8e060a6fe75eac1c0
goakingFy <joaquindiaz66@gmail.com>
2022-10-09 20:12:58 +0200

    Agregado maquetado simple

---
+++ main.js
@@ -0,0 +1,7 @@
+
+// const getData = async ()=>{
+//         let data = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=2&sparkline=false")
+//         let res = await data.json()
+//         console.log(res)
+// }
+// getData()
